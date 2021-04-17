a9e317ff46f9154acf29cc0488ec7701e477b773
Hortensefrom <33423986+Hortensefrom@users.noreply.github.com>
2021-04-17 19:28:28 +0200

    progress
    brand selector + brand data page

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
-from flask import Flask, flash, redirect, render_template, request
+from flask import Flask, flash, redirect, render_template, request, g
 import sqlite3
 
 # Configure application
 app = Flask(__name__)
 
-# Connect to SQLite database
+#Configure database
 
-# Define index route
-@app.route("/", methods=["POST", "GET"])
+def execute_query(query, *args):
+    db = sqlite3.connect('data.db')
+    db.row_factory = sqlite3.Row
+    cur = db.cursor()
+    cur.execute(query, args)
+    result = cur.fetchall()
+    cur.close()
+    return result
+
+# Index route
+@app.route('/')
 def index():
-    connection = sqlite3.connect('data.db')
-    db = connection.cursor()
-
-    if request.method == "POST":
-        brand = request.form.get("brand")
-        db.execute("SELECT * FROM brands WHERE name = ?", brand)
-        return render_template("brand_data.html")
-
-    else:
-        db.execute("SELECT name FROM brands")
-        brands = db.fetchall()
-        return render_template("index.html", brands=brands)
+        result = execute_query('SELECT name FROM brands')
+        return render_template('index.html', brands=result)
 
 
+
+@app.route('/brand_data', methods=['POST', 'GET'])
+def brand_data():
+    if request.method == 'POST':
+        brand_name = request.form.get('brand_name')
+        result = execute_query('SELECT * FROM brands WHERE name = ?', brand_name)
+        return render_template('brand_data.html', result=result)
